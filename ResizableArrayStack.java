cd2fa2b89740ec3407de8edf3b9ff54f5285ecca
lotusbox <95894554+lotusbox@users.noreply.github.com>
2022-03-14 17:01:00 -0700

    Update ResizableArrayStack.java

--- ResizableArrayStack.java
+++ ResizableArrayStack.java
@@ -9,10 +9,17 @@ public class ResizableArrayStack<T> implements StackInterface<T>{
     private static final int DEFAULT_CAPACITY = 50;
     private static final int MAX_CAPACITY = 10000;
 
+    /**
+     * Constructor of an stack with default capacity
+     */
     public ResizableArrayStack(){
         this(DEFAULT_CAPACITY);
     }
 
+    /**
+     * Constructor of a stack with a predefined capacity
+     * @param initialCapacity the capacity
+     */
     public ResizableArrayStack(int initialCapacity){
         integrityOK = false;
         checkCapacity();
@@ -24,6 +31,10 @@ public class ResizableArrayStack<T> implements StackInterface<T>{
         integrityOK = true;
     }
 
+    /**
+     * Puts a new object on top of the stack
+     * @param newEntry the object to be pushed
+     */
     @Override
     public void push(T newEntry) {
         checkIntegrity();
@@ -32,6 +43,9 @@ public class ResizableArrayStack<T> implements StackInterface<T>{
         topIndex++;
     }
 
+    /**
+     * If stack has reached capacity, this function doubles the capacity
+     */
     private void ensureCapacity(){
         if (topIndex >= stack.length - 1){
             int newLength = 2 * stack.length;
@@ -40,6 +54,10 @@ public class ResizableArrayStack<T> implements StackInterface<T>{
         }
     }
 
+    /**
+     * Removes the top object of the stack and returns it
+     * @return the object removed
+     */
     @Override
     public T pop() {
         checkIntegrity();
@@ -53,6 +71,10 @@ public class ResizableArrayStack<T> implements StackInterface<T>{
         }
     }
 
+    /**
+     * Returns the object on top of the stack without removing it
+     * @return the object on top of the stack
+     */
     @Override
     public T peek() {
         checkIntegrity();
@@ -62,11 +84,18 @@ public class ResizableArrayStack<T> implements StackInterface<T>{
             return stack[topIndex];
     }
 
+    /**
+     * checks if the stack is empty
+     * @return returns true if stack is empty
+     */
     @Override
     public boolean isEmpty() {
         return topIndex < 0;
     }
 
+    /**
+     * Empties the stack of all elements
+     */
     @Override
     public void clear() {
         checkIntegrity();
@@ -77,31 +106,19 @@ public class ResizableArrayStack<T> implements StackInterface<T>{
         }
     }
 
+    /**
+     * Ensures array is not out of bounds
+     */
     private void checkIntegrity(){
         if(!integrityOK)
             throw new SecurityException("ArrayList Object is corrupt.");
     }
 
+    /**
+     * Ensurs array has not exceeded maximum capacity
+     */
     private void checkCapacity(){
         if(topIndex > MAX_CAPACITY)
             throw new ArrayIndexOutOfBoundsException("Array capacity has exceeded maximum capacity.");
     }
-
-    public T evaluatePostfix(T anEntryT){
-        return null;
-    }
-
-    /************************* Helpers **********************/
-    private boolean doubleCapacity(){
-        if(topIndex < (MAX_CAPACITY/2)){
-            int newCapacity = 2 * (topIndex+1);
-            stack = Arrays.copyOf(stack, newCapacity);
-            return true;
-        }else{
-            return false;
-        }
-        
-    }
-
-
 }
